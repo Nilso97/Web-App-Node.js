09d81b653510be69745062827b0725f7b121c76c
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-02-03 17:42:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Node JS Servidor
+// Servidor NodeJs
 const fs = require('fs');
 const http = require('http');
 const express = require('express');
@@ -6,11 +6,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const urlencoded = bodyParser.urlencoded({ extended: true });
 
+// Criação do servidor que recebe as solicitações do navegador pela porta TCP 8080
 const servidor = app.listen(8080, function () {
     const porta = servidor.address().port;
     console.log('Servidor executando na porta %s', porta);
 });
 
+// Enviando o conteúdo do 'formulário.html' para o navegador
 app.get('/', function (req, res) {
     fs.readFile('formulario.html', function (erro, dado) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -19,6 +21,7 @@ app.get('/', function (req, res) {
     });
 });
 
+// Carregando a página 'Idade', após o recebimento dos dados enviados da página 'formulário.html'
 app.post('/idade', urlencoded, function (req, res) {
     fs.readFile('idade.html', function (erro, dado) {
         let hoje = new Date();
@@ -27,7 +30,8 @@ app.post('/idade', urlencoded, function (req, res) {
             'anonasc': req.body.anonasc,
             'idade': (hoje.getFullYear() - parseInt(req.body.anonasc))
         };
-
+        
+        // Trocando os valores pelos dados obtidos e processados pelo servidor
         for (let chave in valores) {
             dado = dado.toString().replace('{{' + chave + '}}', valores[chave]);
         }
